70ddda91dad74331fe11efae153ef78c7ffa34ac
Jakob Žagar <jakob.zagar1234@gmail.com>
2026-07-28 22:09:23 +0200

    test: provide browser storage in jsdom

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -3,6 +3,23 @@ import {cleanup} from "@testing-library/react";
 import {afterAll, afterEach, beforeAll} from "vitest";
 import {server} from "./mocks/server";
 
+const storage = new Map<string, string>();
+const localStorageMock: Storage = {
+    get length() {
+        return storage.size;
+    },
+    clear: () => storage.clear(),
+    getItem: (key) => storage.get(key) ?? null,
+    key: (index) => [...storage.keys()][index] ?? null,
+    removeItem: (key) => storage.delete(key),
+    setItem: (key, value) => storage.set(key, String(value)),
+};
+
+Object.defineProperty(globalThis, "localStorage", {
+    configurable: true,
+    value: localStorageMock,
+});
+
 beforeAll(() => server.listen({onUnhandledRequest: "error"}));
 
 afterEach(() => {
