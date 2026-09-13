3f69504231883f52e406a8f27b352372ffd8f990
Jakob Žagar <jakob.zagar1234@gmail.com>
2026-09-13 22:21:18 +0200

    chore: simplify frontend authentication boundary

--- frontend/src/components/layout/footer/Footer.tsx
+++ frontend/src/components/layout/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router";
-import {useAuth} from "../../../features/auth/hooks/useAuth";
+import {useAuth} from "../../../features/auth";
 import {BrandMark} from "../../ui/display/BrandMark";
 import {Icon} from "../../ui/display/Icon";
 
